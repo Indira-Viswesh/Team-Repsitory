5336c4390cb566a9c834b5ad02474792738bec20
Indira-Viswesh <indira.r.viswesh@gmail.com>
2026-09-11 09:15:58 -0700

    Gamify Exploration

--- assets/js/GameEnginev1/GameLevelDesert.js
+++ assets/js/GameEnginev1/GameLevelDesert.js
@@ -521,7 +521,7 @@ class GameLevelDesert {
           }
       };
 
-    const sprite_src_crypto = path + "https://vscode-remote+wsl-002bubuntu-002d24-002e04.vscode-resource.vscode-cdn.net/home/indirav/Gamify-Exploration/images/gamify/Pink_Coin-removebg-preview.jpg?version%3D1789057356833";
+    const sprite_src_crypto = path + "images/gamify/Pink_Coin-removebg-preview.png";
     const sprite_greet_crypto = "*cha-ching*";
     const sprite_data_crypto = {
         id: 'Crypto-NPC',
